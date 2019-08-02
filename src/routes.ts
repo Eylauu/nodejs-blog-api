ddd061e749996f2f948164a4123d6885b1866f8c
Alex Chesnay <me@alex-chesnay.fr>
2019-08-02 12:29:34 +0200

    Création des routes Post et Category

---
+++ src/routes.ts
@@ -0,0 +1,57 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/posts', (req, res) => {
+    let posts: any = [
+        {
+            title: "Post 1",
+            content: "Lorem ipsum...",
+            createdAt: new Date(),
+            updatedAt: null
+        },
+        {
+            title: "Post 2",
+            content: "Lorem ipsum...",
+            createdAt: new Date(),
+            updatedAt: null
+        }
+    ];
+    res.json({ posts: posts });
+})
+
+router.get('/categories', (req, res) => {
+    let categories: any = [
+        {
+            name: "Catégorie 1"
+        }
+        {
+            name: "Catégorie 2"
+        }
+    ];
+    res.json({ categories: categories });
+})
+
+router.route('/post/:postId')
+    .get((req, res) => {
+        res.json({ post: req.params })
+    })
+    .post((req, res) => {
+        res.json({ success: true })
+    })
+    .delete((req, res) => {
+        res.json({ success: true })
+    })
+
+router.route('/category/:categoryId')
+    .get((req, res) => {
+        res.json({ category: req.params })
+    })
+    .post((req, res) => {
+        res.json({ success: true })
+    })
+    .delete((req, res) => {
+        res.json({ success: true })
+    })
+
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from 'express';
+import router from './routes';
 
 export default class Server {
     static start(port: number) {
         const app = express();
         
-        app.get('/', (req, res) => {
-            res.json({ message: "Hello world!" });
-        })
+        // Routes
+        app.use(router);
 
         app.listen(port, () => {
             console.log(`Server started at ${port}.`);
