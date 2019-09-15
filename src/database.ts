d557520843ec2b6e590edcb6f4b68a2d4876f453
Alex Chesnay <me@alex-chesnay.fr>
2019-09-15 02:09:05 +0200

    feat: création d'un fichier d'environement

--- src/database.ts
+++ src/database.ts
@@ -1,15 +1,17 @@
 import mongoose, { Mongoose } from 'mongoose';
 import { MongoError } from 'mongodb';
 
+import "dotenv/config";
+
 export default class Database {
     private static _instance: Database | undefined;
     private _connection: Promise<Mongoose> | undefined;
 
-    private readonly db_host: string = "localhost";
-    private readonly db_port: string = "27017";
-    private readonly db_name: string = "blog_api";
-    private readonly db_user: string = "";
-    private readonly db_password: string = "";
+    private readonly db_host: string | undefined = process.env.DATABASE_HOST;
+    private readonly db_port: string | undefined = process.env.DATABASE_PORT;
+    private readonly db_user: string | undefined = process.env.DATABASE_USER;
+    private readonly db_password: string | undefined = process.env.DATABASE_PASSWORD;
+    private readonly db_name: string | undefined = process.env.DATABASE_NAME;
 
     /**
      * Renvoie une instance de la classe courante (Pattern Singleton)
@@ -28,8 +30,8 @@ export default class Database {
      */
     public get connection(): Promise<Mongoose> {
         if (!this._connection) {
-            const db = `mongodb://${this.db_user}:${this.db_password}@${this.db_host}:${this.db_port}/${this.db_name}`;
-            this._connection = mongoose.connect(db, { useNewUrlParser: true }, (error: MongoError) => {
+            const dbUrl = `mongodb://${this.db_user}:${this.db_password}@${this.db_host}:${this.db_port}/${this.db_name}`;
+            this._connection = mongoose.connect(dbUrl, { useNewUrlParser: true }, (error: MongoError) => {
                 if (error) {
                     console.log(`DATABASE ERROR: ${error}`);
                 } else {
