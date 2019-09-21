8dec7e4ae1ad9a0148077b81adf3efe53094899e
Alex Chesnay <me@alex-chesnay.fr>
2019-09-21 21:35:00 +0200

    feat: configuration du CORS

--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,53 @@
-import express from 'express';
-import router from './routes';
-import Database from './database';
+import express from "express";
+import router from "./routes";
+import Database from "./database";
 
 export default class Server {
-    // Application express
-    private app: express.Application;
+  // Application express
+  private app: express.Application;
 
-    // Port HTTP (par défaut: 3000)
-    private readonly port: number;
+  // Port HTTP (par défaut: 3000)
+  private readonly port: number;
 
-    constructor(port: number = 3000) {
-        this.app = express();
-        this.port = port;
-    }
+  constructor(port: number = 3000) {
+    this.app = express();
+    this.port = port;
+  }
 
-    /**
-     * Lance le server
-     * @return void
-     */
-    public start(): void {
-        // Connexion à la base de données
-        Database.instance.connection;
+  /**
+   * Lance le server
+   * @return void
+   */
+  public start(): void {
+    // Connexion à la base de données
+    Database.instance.connection;
 
-        // Parser
-        this.app.use(express.urlencoded({ extended: true }));
-        this.app.use(express.json());
+    // CORS
+    this.app.use(
+      (
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction
+      ) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header(
+          "Access-Control-Allow-Headers",
+          "Origin, X-Requested-With, Content-Type, Accept"
+        );
+        next();
+      }
+    );
 
-        // Routes
-        this.app.use('/api', router);
+    // Parser
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
 
-        // Lancement du server
-        this.app.listen(this.port, () => console.log(`Server started at ${this.port}.`));
-    }
+    // Routes
+    this.app.use("/api", router);
+
+    // Lancement du server
+    this.app.listen(this.port, () =>
+      console.log(`Server started at ${this.port}.`)
+    );
+  }
 }
